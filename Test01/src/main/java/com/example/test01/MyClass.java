042d253844d69788bc54d5347294785d6c8f6b5d
jkloshhm <jkloshhm@126.com>
2019-06-05 01:18:16 +0800

    2019-06-05：每日一题数组题。

--- Test01/src/main/java/com/example/test01/MyClass.java
+++ Test01/src/main/java/com/example/test01/MyClass.java
@@ -1,7 +1,9 @@
 package com.example.test01;
 
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * @author jkl on 2019/6/4 01:18.  算法习题1
@@ -10,8 +12,9 @@ import java.util.HashMap;
 public class MyClass {
 
     public static void main(String[] args) {
-        int[] a = new int[]{4, 1, 2, 1, 2};
-        System.out.println(singleNumber(a));
+        int[] a = new int[]{4, 1, 2, 1, 5};
+        System.out.println(twoSum(a,9)[0]);
+        System.out.println(twoSum(a,9)[1]);
     }
 
 
@@ -27,14 +30,14 @@ public class MyClass {
      */
     public static int singleNumber(int[] nums) {
         int a = 0;
-        for (int n: nums) {
+        for (int n : nums) {
             a = a ^ n;
         }
         return a;
     }
 
     /**
-     * 《数组题一：存在重复》
+     * 《数组题二：存在重复》
      * 给定一个整数数组，判断是否存在重复元素。
      * <p>
      * 如果任何值在数组中出现至少两次，函数返回 true。如果数组中每个元素都不相同，则返回 false。
@@ -55,7 +58,7 @@ public class MyClass {
      * @return true, false
      */
 
-    private static boolean Test030(int[] nums) {
+    private static boolean test030(int[] nums) {
         if (nums.length < 1) {
             return false;
         }
@@ -69,4 +72,28 @@ public class MyClass {
         }
         return false;
     }
+
+    /**
+     * 《数组题二：两数之和》
+     * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+     * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+     * <p>
+     * 示例:
+     * 给定 nums = [2, 7, 11, 15], target = 9
+     * 因为 nums[0] + nums[1] = 2 + 7 = 9
+     * 所以返回 [0, 1]
+     */
+    private static int[] twoSum(int[] nums, int target) {
+        int[] array = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    array[0] = i;
+                    array[1] = j;
+                }
+            }
+        }
+        return array;
+    }
+
 }
