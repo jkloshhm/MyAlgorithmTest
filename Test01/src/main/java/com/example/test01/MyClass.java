ab62b519bf59f14d1e6ace846b0febb18dcafda8
jkloshhm <jkloshhm@126.com>
2019-06-06 23:14:45 +0800

    2019-06-06：每日一题数组题。

--- Test01/src/main/java/com/example/test01/MyClass.java
+++ Test01/src/main/java/com/example/test01/MyClass.java
@@ -96,4 +96,51 @@ public class MyClass {
         return array;
     }
 
+
+    /**
+     * 《数组题：加一》
+     * 给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。
+     * 最高位数字存放在数组的首位， 数组中每个元素只存储一个数字。
+     * 你可以假设除了整数 0 之外，这个整数不会以零开头。
+     * <p>
+     * 示例 1:
+     * 输入: [1,2,3]
+     * 输出: [1,2,4]
+     * 解释: 输入数组表示数字 123。
+     * <p>
+     * 示例 2:
+     * 输入: [4,3,2,1]
+     * 输出: [4,3,2,2]
+     * 解释: 输入数组表示数字 4321。
+     */
+
+    public static int[] plusOne(int[] digits) {
+
+        if (digits.length < 1) {
+            return digits;
+        }
+
+        for (int i = digits.length-1; i >= 0; i--) {
+            if (digits[i] == 9) {
+                digits[i] = 0;
+            } else {
+                digits[i] = digits[i] + 1;
+                return digits;
+            }
+        }
+
+        if (digits[0] == 0) {
+            int[] a = new int[digits.length + 1];
+            a[0] = 1;
+            for (int i = 1; i < digits.length + 1; i++) {
+                a[i] = 0;
+            }
+            return a;
+        }
+
+        return digits;
+    }
+
+
+
 }
