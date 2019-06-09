bb91b2fc4d2dacf9e9cea59565a6bbd90cbdfdfc
jkloshhm <jkloshhm@126.com>
2019-06-09 23:46:52 +0800

    2019-06-08：每日一题数组题。

--- Test01/src/main/java/com/example/test01/MyClass.java
+++ Test01/src/main/java/com/example/test01/MyClass.java
@@ -13,9 +13,11 @@ import java.util.List;
 public class MyClass {
 
     public static void main(String[] args) {
-        int[] a = new int[]{0, 0, 0, 1, 5};
-        System.out.println(Arrays.toString(moveZeroes(a)));
-        System.out.println(twoSum(a, 9)[1]);
+        int[] a = new int[]{5, 6};
+        int[] b = new int[]{5, 5};
+        //System.out.println(Arrays.toString(moveZeroes(a)));
+        //System.out.println(twoSum(a, 9)[1]);
+        System.out.println(Arrays.toString(intersect(a, b)));
     }
 
 
@@ -174,29 +176,57 @@ public class MyClass {
     /**
      * 两个数组的交集 II
      * 给定两个数组，编写一个函数来计算它们的交集。
-     *
+     * <p>
      * 示例 1:
-     *
+     * <p>
      * 输入: nums1 = [1,2,2,1], nums2 = [2,2]
      * 输出: [2,2]
      * 示例 2:
-     *
+     * <p>
      * 输入: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
      * 输出: [4,9]
      * 说明：
-     *
+     * <p>
      * 输出结果中每个元素出现的次数，应与元素在两个数组中出现的次数一致。
      * 我们可以不考虑输出结果的顺序。
      * 进阶:
-     *
+     * <p>
      * 如果给定的数组已经排好序呢？你将如何优化你的算法？
      * 如果 nums1 的大小比 nums2 小很多，哪种方法更优？
      * 如果 nums2 的元素存储在磁盘上，磁盘内存是有限的，并且你不能一次加载所有的元素到内存中，你该怎么办？
      */
 
-    public int[] intersect(int[] nums1, int[] nums2) {
+    private static int[] intersect(int[] nums1, int[] nums2) {
+        List<Integer> list = new ArrayList<>();
+        if (nums1.length < nums2.length) {
+            for (int i = 0; i < nums1.length; i++) {
+                int k =0;
+                for (int j = k; j < nums2.length; j++) {
+                    if (nums1[i] == nums2[j]) {
+                        list.add(nums1[i]);
+                        break;
+                    } else {
+                        j++;
+                    }
+                }
+            }
+        } else {
+            for (int i = 0; i < nums2.length; i++) {
+                for (int j = 0; j < nums1.length; j++) {
+                    if (nums2[i] == nums1[j]) {
+                        list.add(nums2[i]);
+                    } else {
+                        j++;
+                    }
+                }
+            }
+        }
 
+        int[] a = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            a[i] = list.get(i);
+        }
 
-        return nums1;
+        return a;
     }
 }
